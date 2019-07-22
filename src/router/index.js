30ee589b486e30d2c06e18d006ca8432632bc856
Javvvve <1099046808@qq.com>
2019-07-23 00:27:30 +0800

    首页部分 首页跳转 逻辑未完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import login from '@/components/user_login'
+import login from '../views/user_login'
+import Main from '../views/main'
 
 Vue.use(Router)
 
 export default new Router({
+  mode:'history',     //去掉路由中的#
   routes: [
     { 
-      path: '/login',
+      path: '',
       name: 'user_login',
       component: login
+    },
+    {
+    	//首页
+    	path:'/main',
+    	name:'Main',
+    	component:Main
     }
   ]
 })
